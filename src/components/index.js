79218f94c86c15f241201187e69efd5b9345eb9e
sheyslong <sheilla.silva@ccc.ufcg.edu.br>
2021-01-28 15:26:30 -0300

    Creating project with next and the components for the project

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Footer } from './Footer'
+export { default as GitHubCorner } from './GitHubCorner'
+export { default as QuizBackground } from './QuizBackground'
+export { default as QuizLogo } from './QuizLogo'
+export { default as Widget } from './Widget'
